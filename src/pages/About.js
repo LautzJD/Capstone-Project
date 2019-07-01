2f56492159e7f462c6c903dcef95b38c255f5e90
Justin Lautz <lautzjd@gmail.com>
2019-07-01 17:44:36 -0500

    Updated 'About' link

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,7 +2,7 @@ export default function About(){
     return `
     <div id=contrast>
     <h1>About:</h1>
-    <h2>Hello! My name is Justin Lautz and I am a junior level Web Developer and Savvy Coders Student. This Capstone Project idea came to me about 5 years ago. How? The best ideas come from silly discussions between you, your friends and a few cocktails. This project idea was born from that. We were all a well traveled group and were surprised at how many sports bars/regular bars we knew of that not only supported their local sports teams, but also sports teams that are not local to the area they are in. Let it be because the owner has an affiliation to that out of town team, or that there is just a strong presence of fans in an area that just don’t care for their local teams. We wanted a way to organize and access this data.
+    <h2>Hello! My name is Justin Lautz and I am a Forensic Scientist, Junior Level Web Developer, and Savvy Coders Graduate. This Application idea came to me about 5 years ago. How? The best ideas come from silly discussions between you, your friends and a few cocktails. This project idea was born from that. We were all a well traveled group and were surprised at how many bars we knew of that not only supported their local sports teams, but also sports teams that are not local to the area they are in. Let it be because the owner has an affiliation to that out of town team, or that there is just a strong presence of fans in an area that just don’t care for their local teams. We wanted a way to organize and access this data.
     </div>
     `;
 }
